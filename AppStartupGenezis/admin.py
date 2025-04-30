c76f9dc1579751f2f9de750bacd3a9a5b8ab4ff5
Luciano Cortez <163349460+Lucianodavidcor@users.noreply.github.com>
2025-04-29 23:41:55 -0300

    Conexion a base de datos, con supabase
    Tambien creamos los modelos
    
    Co-Authored-By: Fernando Alma <89047765+fernando-alma@users.noreply.github.com>

--- AppStartupGenezis/admin.py
+++ AppStartupGenezis/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Paciente, Medico, Institucion, Turno, HistorialMedico
+
+admin.site.register(Paciente)
+admin.site.register(Medico)
+admin.site.register(Institucion)
+admin.site.register(Turno)
+admin.site.register(HistorialMedico)
--- AppStartupGenezis/apps.py
+++ AppStartupGenezis/apps.py
@@ -4,3 +4,10 @@ from django.apps import AppConfig
 class AppstartupgenezisConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'AppStartupGenezis'
+
+class AppStartupGeneralsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'AppStartupGenerals'
+
+    def ready(self):
+        import AppStartupGenezis.signals  # <- Asegurate de importar signals aquí
